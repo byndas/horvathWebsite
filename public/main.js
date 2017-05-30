7c60336d9ce5beefe25dc4959a91c32ba642b247
Marc Byndas <byndasma@gmail.com>
2017-05-30 04:09:00 +0000

    google map

--- public/main.js
+++ public/main.js
@@ -1,1 +1,17 @@
 'use strict'
+
+// function initialize() {
+//     var mapOptions = {
+//         zoom: 8,
+//         center: new google.maps.LatLng()
+//     };
+//     var map = new google.maps.Map(
+//         document.getElementById('map-canvas'), mapOptions);
+// }
+
+// function loadScript() {
+//     var script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 'callback=initialize';
+//     document.body.appendChild(script);
+// }
